67c91c68afae0953105d559c9221bf1517133a94
vasvince98 <87307436+vasvince98@users.noreply.github.com>
2023-03-23 22:41:45 +0100

    Utolsó utáni

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -26,7 +26,7 @@ function BookCreate( {onCreate} ) {
                 <input className="input" value={user} onChange={handleUserChange}/>
                 <label>Sportág</label>
                 <input className="input" value={title} onChange={handleTitleChange}/>
-                <button className="button">Hozzáad!</button>
+                <button className="button">Hozzáadd!</button>
             </form>
         </div>
     );
